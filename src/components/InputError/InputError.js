c907a06c1d3c60bf0a79fd073302fd1427034587
Rizchel Wood <rizchelwood@gmail.com>
2019-07-29 15:35:54 -0600

    Add jsx pragma to form elements to support v10

--- src/components/InputError/InputError.js
+++ src/components/InputError/InputError.js
@@ -1,26 +1,27 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx } from "@emotion/core"
 import PropTypes from "prop-types"
 
 import { spaces, palette, fontSizes } from "../../utils/presets"
 
 const InputError = ({ children }) => (
-    <p
-      css={{
-        color: palette.red[600],
-        fontSize: fontSizes.xs,
-        lineHeight: `1`,
-        padding: `0`,
-        margin: `${spaces.xs} ${spaces[`2xs`]} 0`,
-        svg: {
-          color: `current-color`,
-          marginRight: spaces[`2xs`],
-          verticalAlign: `text-top`,
-        },
-      }}
-    >
-      {children}
-    </p>
-  )
+  <p
+    css={{
+      color: palette.red[600],
+      fontSize: fontSizes.xs,
+      lineHeight: `1`,
+      padding: `0`,
+      margin: `${spaces.xs} ${spaces[`2xs`]} 0`,
+      svg: {
+        color: `current-color`,
+        marginRight: spaces[`2xs`],
+        verticalAlign: `text-top`,
+      },
+    }}
+  >
+    {children}
+  </p>
+)
 
 InputError.propTypes = {
   children: PropTypes.node,
