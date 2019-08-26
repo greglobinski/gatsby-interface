3fd784efd15fbe652a43fc5d62be95ffce5da362
greglobinski <greglobinski@gmail.com>
2019-08-26 22:32:34 +0200

    fix: remove className

--- src/components/SidebarNav/SidebarNav.js
+++ src/components/SidebarNav/SidebarNav.js
@@ -7,7 +7,7 @@ import PropTypes from "prop-types"
 import colors from "../../theme/colors"
 import { spaces, fontFamilies, fontSizes } from "../../utils/presets"
 
-const SidebarNav = ({ children, options, style, className, ...rest }) => (
+const SidebarNav = ({ children, options, style, ...rest }) => (
   <nav
     css={{
       paddingLeft: spaces.xl,
