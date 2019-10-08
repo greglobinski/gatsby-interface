251e47b390bd98cd2129b74dc447c631b31637ed
Aleksei Ustiuzhanin <elanhant@gmail.com>
2019-10-08 13:56:13 -0700

    Readme for CopyButton

--- src/components/CopyButton/CopyButton.stories.tsx
+++ src/components/CopyButton/CopyButton.stories.tsx
@@ -5,9 +5,33 @@ import { text, boolean, radios } from "@storybook/addon-knobs"
 
 import { StoryUtils } from "../../utils/storybook"
 import CopyButton from "./CopyButton"
+import README from "./README.md"
+import { action } from "@storybook/addon-actions"
 
-storiesOf(`CopyButton`, module).add(`Default`, () => (
-  <StoryUtils.Container>
-    <CopyButton content="Hello world!" />
-  </StoryUtils.Container>
-))
+storiesOf(`CopyButton`, module)
+  .addParameters({
+    options: {
+      showPanel: true,
+    },
+    readme: {
+      sidebar: README,
+      includePropTables: [CopyButton],
+    },
+  })
+  .add(`Default`, () => {
+    const labelCopy = text(`Label (default)`, `Copy`)
+    const labelCopied = text(`Label (copied)`, `Copied!`)
+    const titleCopy = text(`Title (default)`, `Copy content`)
+    const titleCopied = text(`Label (copied)`, `Content copied!`)
+
+    return (
+      <StoryUtils.Container>
+        <CopyButton
+          content={text(`content`, `Hello world!`)}
+          onClick={action(`Copy`)}
+          getButtonLabel={copied => (copied ? labelCopied : labelCopy)}
+          getButtonTitle={copied => (copied ? titleCopied : titleCopy)}
+        />
+      </StoryUtils.Container>
+    )
+  })
