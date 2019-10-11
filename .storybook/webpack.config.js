9c1c57469c6c16b4795ab343a2c0bfb9d4d1bd8c
Aleksei Ustiuzhanin <elanhant@gmail.com>
2019-10-11 11:39:04 -0700

    Preserve quotes in Storybook webpack config attempt 2

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,4 @@
-/* eslint-disable quotes */
+// prettier-ignore
 module.exports = async ({ config, mode }) => {
   // console.dir(config.plugins, { depth: null }) || config;
 
@@ -10,39 +10,39 @@ module.exports = async ({ config, mode }) => {
   // The next two lines should always reflect the config in jest-preprocess.js until there is a way for Gatsby to expose an internal webpack.config
   // use @babel/preset-react for JSX and env (instead of staged presets)
   config.module.rules[0].use[0].options.presets = [
-    require.resolve("@babel/preset-react"),
-    require.resolve("@babel/preset-env"),
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-env'),
   ]
 
   // use @babel/plugin-proposal-class-properties for class arrow functions
   config.module.rules[0].use[0].options.plugins = [
-    require.resolve("babel-plugin-react-docgen"),
-    require.resolve("@babel/plugin-proposal-class-properties"),
-    require.resolve("babel-plugin-emotion"),
-    require.resolve("@babel/plugin-proposal-object-rest-spread"),
+    require.resolve('babel-plugin-react-docgen'),
+    require.resolve('@babel/plugin-proposal-class-properties'),
+    require.resolve('babel-plugin-emotion'),
+    require.resolve('@babel/plugin-proposal-object-rest-spread'),
   ]
 
   config.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
-    type: "javascript/auto",
+    type: 'javascript/auto',
   })
 
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve("babel-loader"),
+    loader: require.resolve('babel-loader'),
   })
 
   config.module.rules.push({
     test: /\.stories\.jsx?$/,
-    loaders: [require.resolve("@storybook/addon-storysource/loader")],
-    enforce: "pre",
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre',
   })
 
   config.resolve.extensions = [
-    ".mjs",
-    ".ts",
-    ".tsx",
+    '.mjs',
+    '.ts',
+    '.tsx',
     ...config.resolve.extensions,
   ]
 
