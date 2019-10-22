b007d43c8ae7dd75bc4ea8e32c6055a8642c4776
Amberley Romo <amberleyjohanna@gmail.com>
2019-10-22 16:06:52 -0500

    handle outside clicks

--- src/components/skeletons/BaseNavigation/BaseNavigation.js
+++ src/components/skeletons/BaseNavigation/BaseNavigation.js
@@ -1,8 +1,30 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core"
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { Link } from "gatsby"
 // import { visuallyHidden } from "../../../utils/helpers"
+
+const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = event => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) {
+        return
+      }
+
+      handler(event)
+    }
+    if (document) {
+      document.addEventListener(`mousedown`, listener)
+      document.addEventListener(`touchstart`, listener)
+
+      return () => {
+        document.removeEventListener(`mousedown`, listener)
+        document.removeEventListener(`touchstart`, listener)
+      }
+    }
+  }, [ref, handler])
+}
 
 const BaseNavigationStyles = {}
 
@@ -46,9 +68,9 @@ const BaseNavigationItemStyles = {
   display: `inline-block`,
   position: `relative`,
   // Show dropdown menu on hover, if exists
-  "&:hover > ul": {
-    ...DropdownListStyles,
-  },
+  // "&:hover > ul": {
+  //   ...DropdownListStyles,
+  // },
   // ":focus-within > ul": {
   //   ...DropdownListStyles,
   // },
@@ -57,14 +79,41 @@ const BaseNavigationItemStyles = {
 BaseNavigation.Item = ({ children, navItem: { linkTo, subItems = [] } }) => {
   const [isDropdownOpen, toggleDropdown] = useState(false)
   console.log({ isDropdownOpen })
+
+  const itemHasDropdown = subItems.length > 0
+  let ref
+  if (itemHasDropdown) {
+    ref = useRef()
+    // Call hook passing in the ref and a function to call on outside click
+    useOnClickOutside(ref, () => {
+      console.log(`running click outside`)
+      toggleDropdown(false)
+    })
+  }
+
   return (
-    <li css={{ ...BaseNavigationItemStyles }}>
+    <li
+      ref={ref}
+      css={{
+        ...BaseNavigationItemStyles,
+      }}
+    >
       <Link to={linkTo}>{children}</Link>
-      {subItems.length > 0 && (
-        <button onClick={() => toggleDropdown(!isDropdownOpen)}>&or;</button>
+      {itemHasDropdown && (
+        <button
+          onClick={() => {
+            console.log(`running onClick`)
+            toggleDropdown(!isDropdownOpen)
+          }}
+        >
+          &or;
+        </button>
       )}
-      {subItems.length > 0 && isDropdownOpen && (
-        <BaseNavigation.Dropdown items={subItems} />
+      {itemHasDropdown && isDropdownOpen && (
+        <BaseNavigation.Dropdown
+          isDropdownOpen={isDropdownOpen}
+          items={subItems}
+        />
       )}
     </li>
   )
@@ -77,15 +126,20 @@ const BaseNavigationDropdownStyles = {
   display: `none`,
 }
 
-BaseNavigation.Dropdown = ({ items }) => (
-    <ul css={{ ...BaseNavigationDropdownStyles }}>
-      {items.map(item => (
-        <BaseNavigation.DropdownItem item={item}>
-          {item.name}
-        </BaseNavigation.DropdownItem>
-      ))}
-    </ul>
-  )
+BaseNavigation.Dropdown = ({ items, isDropdownOpen }) => (
+  <ul
+    css={{
+      ...BaseNavigationDropdownStyles,
+      ...(isDropdownOpen && DropdownListStyles),
+    }}
+  >
+    {items.map(item => (
+      <BaseNavigation.DropdownItem item={item}>
+        {item.name}
+      </BaseNavigation.DropdownItem>
+    ))}
+  </ul>
+)
 
 BaseNavigation.DropdownItem = ({ children, item: { linkTo } }) => (
   <li>
