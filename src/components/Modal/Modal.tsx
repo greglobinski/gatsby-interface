39166dadab7efd3a4c4ffa70136d9de398dc49fa
Marvin Frachet <marvin.frachet@gmail.com>
2019-11-04 15:39:39 +0100

    Adding declarative modals (#93)

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,57 @@
+import React from "react"
+import { DialogOverlay, DialogContent, DialogOverlayProps } from "@reach/dialog"
+import styled from "@emotion/styled"
+import { palette } from "../../utils/presets"
+import { hexToRGBA } from "../../utils/helpers/hexToRgb"
+import { keyframes } from "@emotion/core"
+
+import "@reach/dialog/styles.css"
+
+export type ModalType = "success" | "info" | "warn" | "error"
+
+const buildFadeIn = (color: string) =>
+  keyframes`
+    0% {
+       background-color: transparent;
+     }
+   
+     100% {
+       background-color: ${color};
+     }
+   `
+
+const successFade = buildFadeIn(hexToRGBA(palette.green[`500`], 0.75))
+const errorFade = buildFadeIn(hexToRGBA(palette.red[`500`], 0.75))
+const infoFade = buildFadeIn(hexToRGBA(palette.purple[`500`], 0.75))
+const warnFade = buildFadeIn(hexToRGBA(palette.orange[`500`], 0.75))
+
+export interface ModalProps extends Omit<DialogOverlayProps, "ref"> {
+  type?: ModalType
+}
+
+const getBackgroundAnimation = (type?: ModalType) => {
+  if (type === `error`) return errorFade
+  if (type === `warn`) return warnFade
+  if (type === `success`) return successFade
+
+  return infoFade
+}
+
+const Overlay = styled(DialogOverlay)<
+  DialogOverlayProps & { animation: ReturnType<typeof buildFadeIn> }
+>`
+  animation: ${props => props.animation} 0.5s ease forwards;
+`
+
+const Content = styled(DialogContent)`
+  margin: 0;
+  padding: 0;
+  background: transparent;
+  width: auto;
+`
+
+export const Modal: React.FC<ModalProps> = ({ children, type, ...props }) => (
+  <Overlay {...props} animation={getBackgroundAnimation(type)}>
+    <Content>{children}</Content>
+  </Overlay>
+)
---
+++ src/components/Modal/ModalFullScreen.ts
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled"
+import { keyframes } from "@emotion/core"
+import { palette } from "../../utils/presets"
+
+const rotationIncoming = keyframes`
+  100% {
+     transform: translate(0, 0) scale(1) perspective(1000px) rotateX(0);
+  }
+`
+
+export const ModalFullScreen = styled.div`
+  animation: ${rotationIncoming} 0.5s 0.25s ease forwards;
+  background: ${palette.white};
+  height: 100vh;
+  width: 100%;
+  outline: none;
+  overflow-y: auto;
+  overflow-x: hidden;
+  position: relative;
+  transform: translate(0, 150vh) scale(0.9) perspective(1000px) rotateX(-90deg);
+  transform-origin: top center;
+`
