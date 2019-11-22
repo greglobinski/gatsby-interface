a5265567427cae47eb30a1e33c2a031134ccee3d
Josh Comeau <joshwcomeau@gmail.com>
2019-11-22 11:28:04 -0500

    chore: use 'pure' random values in tests

--- __tests__/Storyshots.test.js
+++ __tests__/Storyshots.test.js
@@ -7,9 +7,4 @@ initStoryshots({
     const { container } = render(storyElement, rendererOptions)
     return container
   },
-  /**
-   * We have to exclude DecorativeDots from snapshots since they are always rendered in a differnet way,
-   * which makes snapshot testing useless
-   */
-  storyKindRegex: /^((?!.*?DecorativeDots).)*$/,
 })
