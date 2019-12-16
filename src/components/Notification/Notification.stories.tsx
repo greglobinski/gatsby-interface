6389198559bac9be4ef432987c2383ef1f17958e
Aleksei Ustiuzhanin <elanhant@gmail.com>
2019-12-16 09:14:33 -0800

    chore: add animated Notification story

--- src/components/Notification/Notification.stories.tsx
+++ src/components/Notification/Notification.stories.tsx
@@ -3,6 +3,7 @@ import { jsx } from "@emotion/core"
 import React from "react"
 import { storiesOf } from "@storybook/react"
 import { radios } from "@storybook/addon-knobs"
+import { useTransition, animated } from "react-spring"
 import { Notification, NotificationTone, NotificationProps } from "."
 import { StoryUtils } from "../../utils/storybook"
 import { Button } from "../Button"
@@ -83,3 +84,51 @@ storiesOf(`Notification`, module)
       </div>
     </StoryUtils.Container>
   ))
+  .add(`animated with react-spring`, () => {
+    const ReactSpringNotification = animated(Notification)
+
+    function AnimatedNotification(props: NotificationProps) {
+      const [isOpened, setIsOpened] = React.useState<boolean>(true)
+
+      const transitions = useTransition(isOpened, null, {
+        from: { opacity: 0, transform: "translate3d(0, -40px, 0)" },
+        enter: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+        leave: { opacity: 0, transform: "translate3d(0, 40px, 0)" },
+      })
+
+      return (
+        <React.Fragment>
+          <Button
+            css={{ marginBottom: `1rem` }}
+            onClick={() => setIsOpened(!isOpened)}
+          >
+            Toggle notification
+          </Button>
+
+          {transitions.map(
+            ({ item, key, props: style }) =>
+              item && (
+                <ReactSpringNotification
+                  key={key}
+                  style={style}
+                  onDismissButtonClick={() => setIsOpened(false)}
+                  {...props}
+                />
+              )
+          )}
+        </React.Fragment>
+      )
+    }
+
+    return (
+      <StoryUtils.Container>
+        <div css={{ maxWidth: "400px", width: "100%", minHeight: "300px" }}>
+          <AnimatedNotification
+            css={{ marginTop: `1rem` }}
+            tone={radios(`tone`, tonesOptions, `BRAND`)}
+            content={`Notification variant 'PRIMARY' with close`}
+          />
+        </div>
+      </StoryUtils.Container>
+    )
+  })
