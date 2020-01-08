0b9680293c1f3ef0ccb48e22a09ac30fc55e64a5
greg lobinski <greglobinski@gmail.com>
2020-01-08 04:03:14 +0100

    chore: remove comment

--- rollup.config.js
+++ rollup.config.js
@@ -37,9 +37,6 @@ export default {
     resolve({ extensions }),
     commonjs({
       namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
         "node_modules/formik/node_modules/scheduler/index.js": [
           "unstable_runWithPriority",
           "LowPriority",
