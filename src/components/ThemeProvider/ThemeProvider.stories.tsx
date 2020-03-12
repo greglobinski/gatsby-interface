88478c1190981f698feb1f693906ceb79e82d004
Marvin Frachet <marvin@gatsbyjs.com>
2020-03-12 07:51:17 +0100

    Allow theme provider to receive a theme property (#227)

--- src/components/ThemeProvider/ThemeProvider.stories.tsx
+++ src/components/ThemeProvider/ThemeProvider.stories.tsx
@@ -3,7 +3,7 @@ import { jsx, Interpolation } from "@emotion/core"
 import { storiesOf } from "@storybook/react"
 import { StoryUtils } from "../../utils/storybook"
 import { css } from "@emotion/core"
-import { Theme } from "../../theme"
+import { Theme, getTheme } from "../../theme"
 import { useTheme, ThemeProvider } from "."
 
 const textCss: Interpolation = {
@@ -65,6 +65,71 @@ storiesOf(`ThemeProvider`, module)
 
     return (
       <ThemeProvider>
+        <TestComponent />
+      </ThemeProvider>
+    )
+  })
+  .add("ThemeProvider custom theme prop of type Theme", () => {
+    function TestComponent() {
+      return (
+        <StoryUtils.Container>
+          <StoryUtils.Stack>
+            <div
+              css={(theme: Theme) =>
+                css(textCss, {
+                  backgroundColor: theme.colors.gatsby,
+                  color: theme.colors.white,
+                  padding: theme.space[5],
+                })
+              }
+            >
+              Lorem ipsum
+            </div>
+          </StoryUtils.Stack>
+        </StoryUtils.Container>
+      )
+    }
+
+    const defaultTheme = getTheme()
+    const otherTheme = {
+      ...defaultTheme,
+      colors: { ...defaultTheme.colors, gatsby: "red" },
+    }
+
+    return (
+      <ThemeProvider theme={otherTheme}>
+        <TestComponent />
+      </ThemeProvider>
+    )
+  })
+  .add("ThemeProvider custom theme prop of type function", () => {
+    function TestComponent() {
+      return (
+        <StoryUtils.Container>
+          <StoryUtils.Stack>
+            <div
+              css={(theme: Theme) =>
+                css(textCss, {
+                  backgroundColor: theme.colors.gatsby,
+                  color: theme.colors.white,
+                  padding: theme.space[5],
+                })
+              }
+            >
+              Lorem ipsum
+            </div>
+          </StoryUtils.Stack>
+        </StoryUtils.Container>
+      )
+    }
+
+    const getNewTheme = (defaultTheme: Theme) => ({
+      ...defaultTheme,
+      colors: { ...defaultTheme.colors, gatsby: "yellow" },
+    })
+
+    return (
+      <ThemeProvider theme={getNewTheme}>
         <TestComponent />
       </ThemeProvider>
     )
--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { ThemeProvider as ThemeUiProvider, useThemeUI } from 'theme-ui'
-import { getTheme, Theme } from '../../theme'
+import React from "react"
+import { ThemeProvider as ThemeUiProvider, useThemeUI } from "theme-ui"
+import { getTheme, Theme } from "../../theme"
 
 export type ThemeProviderProps = {
-    children?: React.ReactNode;
+  children?: React.ReactNode
+  theme?: Theme | ((baseTheme: Theme) => Theme)
 }
 
-export default function ThemeProvider({ children }: ThemeProviderProps) {
-    const theme = React.useMemo(() => getTheme(), [])
-    
-    return  (
-        <ThemeUiProvider<Theme> theme={theme}>{children}</ThemeUiProvider>
-      )
+export default function ThemeProvider({ children, theme }: ThemeProviderProps) {
+  const themeValue = React.useMemo(() => {
+    const baseTheme = getTheme()
+
+    if (typeof theme === "function") {
+      return theme(baseTheme)
+    }
+
+    return theme || baseTheme
+  }, [theme, getTheme])
+
+  return <ThemeUiProvider<Theme> theme={themeValue}>{children}</ThemeUiProvider>
 }
 
 // To distinguish from Theme UI's ThemeProvider
-ThemeProvider.displayName = 'GatsbyInterfaceThemeProvider'
+ThemeProvider.displayName = "GatsbyInterfaceThemeProvider"
 
 export function useTheme() {
-    // @ts-ignore
-    return useThemeUI().theme as Theme;
+  // @ts-ignore
+  return useThemeUI().theme as Theme
 }
