d2b5bcc68a3842c7e753b488fab458e538bcaace
Aleksei Ustiuzhanin <elanhant@gmail.com>
2020-03-18 11:45:11 -0700

    fix: do not import Reach dialog css

--- src/components/Modal/components.tsx
+++ src/components/Modal/components.tsx
@@ -2,6 +2,7 @@ import { DialogContent } from "@reach/dialog"
 import styled from "@emotion/styled"
 
 export const Content = styled(DialogContent)`
+  outline: none;
   margin: 0;
   padding: 0;
   background: transparent;
